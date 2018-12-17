3e8bba7138bb5425b72e8205c3fa160a9179443e
Danielxburns <danielburnsart@gmail.com>
2018-12-17 15:43:08 -0600

    set new keynames to timestamp.

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
 
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
+    localStorage.setItem(new Date(), $('.text-entry').val());
+    var myItemInStorage = localStorage.getItem(new Date());
+    console.log(new Date(), myItemInStorage);
+    // create new key from timestamp
+
 
     // display the value here
     $('.list-display-field').text(myItemInStorage); // ??
@@ -16,5 +18,7 @@ $(document).ready(function(){
   $('.btn-delete').on('click', function(){
     localStorage.removeItem('inputFieldValue');
   });
+
+  // delete all storage
 
 });
